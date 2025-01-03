28c026ab3789f2f8c4a12028a0bc60ac497050bc
Your Name <you@example.com>
2025-01-04 06:39:12 +0700

    news

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+        <div className='pt-6'>
+            <h3 className='font-bold text-xl md:text-2xl tracking-tight mb-4 mt-8 text-black dark:text-white'>
+                I am a Chainlinker, a holder. Chainlink is inevitable.
+            </h3>
+        </div>
+    )
+}
